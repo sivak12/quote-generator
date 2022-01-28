bfbc7aafd19c63cbc36ff8e7957c84d8bd03b231
Sivakumaran E <sivakumaran.e@gmail.com>
2022-01-28 12:09:44 +0530

    less db refreshes

--- server.js
+++ server.js
@@ -14,15 +14,31 @@ mongoose.connect(db_conn, {
 
 app.set('view engine', 'ejs');
 
+let quotes_list = [];
+let quotes_fetched = false;
+
 app.get('/', async (req, res) => {
     
+    if (!quotes_fetched) {
+        quotes_list = await Quote.find();
+        quotes_fetched = true;
+    }
+    const rand = Math.floor(Math.random() * quotes_list.length);
+    const quote = quotes_list[rand];
+
+    
     // find a random quote
-    const numQuotes = await Quote.estimatedDocumentCount();
-    const rand = Math.floor(Math.random() * numQuotes);
-    const quote = await Quote.findOne().skip(rand);
+    //const numQuotes = await Quote.estimatedDocumentCount();
+    //const rand = Math.floor(Math.random() * numQuotes);
+    //const quote = await Quote.findOne().skip(rand);
 
     console.log('quote:'+ quote);
     res.render('index', { quote: quote });
 });
 
+// async function getAllQuotes() {
+//     quotes_list = await Quote.find();
+// }
+
 app.listen(process.env.PORT || 5000);
+
